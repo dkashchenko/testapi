afb54aeccceac3273e7ee0b33fed5f811280cac7
dkashchenko <dkwork7@gmail.com>
2019-12-03 23:34:52 +0000

    Dal...

---
+++ TestAPI.DAL/TestAPI.DAL/Data/TestAPIDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestAPI.Model.Models;
+
+namespace TestAPI.DAL.Data
+{
+    class TestAPIDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=library;user=user;password=password");
+        }
+
+        public DbSet<Product> Product;
+    }
+}
---
+++ TestAPI.DAL/TestAPI.DAL/Repository/Product/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAPI.DAL.Repository.Product
+{
+    interface IProductRepository
+    {
+        Task<IList<TestAPI.Model.Models.Product>> GetAllAsync();
+
+        Task<TestAPI.Model.Models.Product> GetByIdAsync();
+
+    }
+}
