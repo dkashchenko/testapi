f9b3f0fa2f2c7c47bfc0974453087c8a9c6456c9
dkashchenko <dkwork7@gmail.com>
2019-12-03 22:52:24 +0000

    TestAPI Model

---
+++ TestAPI.Model/TestAPI.Model/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestAPI.Model.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Price")]
+        public double Price { get; set; }
+    }
+}
